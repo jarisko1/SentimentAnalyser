36a17e74ab7f3338920e4ea2a420071f9eb73a21
Jarisko <jarisko@pm.me>
2024-11-07 21:05:58 +0100

    version 0.02 prompt engineering, some file upload options

--- openai_lib.py
+++ openai_lib.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import json
 
 OPENAI_MODEL = "gpt-4o-mini"
 OPENAI_TEMPERATURE = 0.7
@@ -22,27 +23,65 @@ def calculate_prompt_price(input_tokens: int, output_tokens: int) -> int:
 def analyze_sentiment(client:openai.OpenAI, input:str) -> str:
     """Analyzes sentiment of the input and returns formatted output in Markdown"""
 
+    topics_to_watch = "\n".join([
+        "General tone",
+        "Expressed strong emotions",
+        "Trends in conversation sentiment over time",
+        # "Conversation topics and their frequency",
+        "Emotionally triggered words",
+        "Emerging concerns",
+        # "Language patterns",
+        "Feeling included or excluded",
+        "Discrimination",
+        "Harassment",
+        "Mentioned overload or burnour",
+        "Work-life balance",
+        "Requests for help or support",
+        "Providing feedback",
+        "Recevied feedback",
+        "Alignment with company vision",
+        "Trust in company decisions",
+        "Transparency in communication",
+        "Innovation",
+        "Motivation in work",
+        "Turnover-related langauge",
+        "Options in career development"
+    ])
+                    
     messages=[{"role": "system", 
-               "content": "You are an expert in analysing sentiment in human interaction."
+               "content": "You are an expert in analysing sentiment and mood in human interaction."
                "User will input excerpt from a conversation."
-               "Extract people in the conversation, topics discusses and sentiment of each person per topic."
-               "Provide concise answer."},
+               "Extract language of the conversation and people involved directly or indirectly."
+               "Watch for following topics:"
+               f"{topics_to_watch}"
+               "Do not make up facts that are not in the conversation"
+               "Provide concise answer in form of a JSON which will have follwing keys:"
+               "language_used: language of the conversation"
+               "people_involved: list of all people involved, in format person name: directly/indirectly"
+               "topics: list of all topics to watch, with key for every person involved under the topic."
+               "Output only topics for which there is relevant information in the conversation."},
               {"role": "user", 
                "content": input}]
 
     assistant_reponse = client.chat.completions.create(
         messages=messages,
         model=OPENAI_MODEL,
-        temperature=OPENAI_TEMPERATURE
+        temperature=OPENAI_TEMPERATURE,
+        response_format={"type": "json_object"}
     )
     
     response_text = assistant_reponse.choices[0].message.content
 
+    response_dict = json.loads(response_text)
+
     input_cost = calculate_prompt_price(assistant_reponse.usage.prompt_tokens, assistant_reponse.usage.completion_tokens) * 100
+    response_dict["cost_in_cents"] = input_cost
 
-    return f"""## Sentiment Analysis
+    return response_dict
 
-{response_text}
----
-Analysis cost: {input_cost:.4f} USD cents
-"""
+#     return f"""## Sentiment Analysis
+
+# {response_text}
+# ---
+# Analysis cost: {input_cost:.4f} USD cents
+# """
--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# import pandas as pd
+import pandas as pd
 import importlib
 
 import openai_lib as oai
@@ -31,16 +31,17 @@ text_input = st.text_area(label="Enter text", value="""[16:45, 03/11/2024] Peter
 [17:35, 03/11/2024] Pavol: Ale viac asi ne
 [17:41, 03/11/2024] Peter: No oki jak povies
 [17:41, 03/11/2024] Peter: Ale to potom ked pozeram lama requirements rpzbehame asi tak ten najmensi model
-[17:42, 03/11/2024] Peter: Zajtra mozme jaris pokecat
+[17:42, 03/11/2024] Peter: Zajtra mozme Pavol pokecat
 [21:51, 03/11/2024] Denis: ja by som zas povedal ze ked mame 30k na server tak spravme future proof ready a najebme tam co to da😂
 [09:48, 04/11/2024] Peter: Caute
 [09:48, 04/11/2024] Peter: Na stredu dam nas meeting skusim aj Matildu zlanarit nan
-[09:49, 04/11/2024] Peter: Jeden topic co zatial nemam sajnu neviete okrem tomiho nejaky tool nacom by sme vedeli nasimulovat mock/demo pre ten sentiment check?
+[09:49, 04/11/2024] Peter: Jeden topic co zatial nemam sajnu neviete okrem Denisa nejaky tool nacom by sme vedeli nasimulovat mock/demo pre ten sentiment check?
 [09:49, 04/11/2024] Peter: Nejaky video editor alebo nieco co aluzi na vytvaranie takychto demo videi
 [09:51, 04/11/2024] Pavol: Caves. Ja v stredu nemozem, ibaze by sme si dali meeting v ofise""", )
 file_input = st.file_uploader(label="Enter file", accept_multiple_files=True)
 
 button_input = st.button(label="Analyse sentiment")
+
 
 # Process input
 if button_input and (text_input or file_input):
@@ -51,7 +52,23 @@ if button_input and (text_input or file_input):
         text_to_analyze += text_input
     
     if file_input:
-        pass
+        for file in file_input:
 
-    # st.text(oai.analyze_sentiment(client, text_to_analyze))
-    st.markdown(oai.analyze_sentiment(client, text_to_analyze), unsafe_allow_html=True)
+            if file.type == "text/plain":
+                text_file = file.read().decode("utf-8")
+
+            if file.type == "text/csv":
+                csv_df = pd.read_csv(file, header=None)
+                
+            if file.type == "image/jpeg":
+                # TODO: implement OCR
+                image_file = file.read()
+
+            if file.type == "application/pdf":
+                # TODO: implement
+                pass
+
+
+
+    st.write(oai.analyze_sentiment(client, text_to_analyze))
+    # st.markdown(oai.analyze_sentiment(client, text_to_analyze), unsafe_allow_html=True)
